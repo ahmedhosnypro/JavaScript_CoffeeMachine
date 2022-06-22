d4dce1dfccfc2c449003777e47cb5bdba78ccd15
ahmedhosnypro <ahhosnyas@gmail.com>
2022-06-22 12:07:39 +0200

    Coffee for all

--- Coffee Machine/task/main.js
+++ Coffee Machine/task/main.js
@@ -1,18 +1,29 @@
 const input = require('sync-input')
 
-start();
+calcNumberOfServings();
 
-function start() {
-    console.log(`Write how many cups of coffee you will need:`);
+function calcNumberOfServings() {
+    console.log("\"Write how many ml of water the coffee machine has:");
+    let waterV = Number(input());
 
-    let cupsNum = Number(input());
-    calcIngredients(cupsNum);
-}
+    console.log("Write how many grams of coffee beans the coffee machine has:");
+    let milkV = Number(input());
 
+    console.log("Write how many cups of coffee you will need:");
+    let coffeeM = Number(input());
 
-function calcIngredients(cups) {
-    console.log("For 25 cups of coffee you will need:");
-    console.log((cups * 200).toString() + " ml of water");
-    console.log((cups * 50).toString() + " ml of milk");
-    console.log((cups * 15).toString() + " g of coffee beans");
+    console.log("Write how many cups of coffee you will need:");
+    let wantedCups = Number(input());
+
+    let cupsAvailable = (waterV / 200) >> 0;
+    cupsAvailable = Math.max(cupsAvailable, (milkV / 50) >> 0);
+    cupsAvailable = Math.max(cupsAvailable, (coffeeM / 15) >> 0);
+
+    if (cupsAvailable === wantedCups) {
+        console.log("Yes, I can make that amount of coffee\n")
+    } else if (cupsAvailable < wantedCups) {
+        console.log("No, I can make only $cupsAvailable cup(s) of coffee")
+    } else {
+        console.log(`Yes, I can make that amount of coffee (and even ${cupsAvailable - wantedCups} more than that)`);
+    }
 }
